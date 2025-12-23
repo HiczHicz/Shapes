a7d8b4a1552ee7fd6c57c001c5e25676ea2d9ba3
Tynka <martyna05p@gmail.com>
2025-12-24 00:24:57 +0100

    task II (1), close button, credits button

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,45 @@
-import java.awt.Color;
+//import java.awt.Color;
+//
+//import pl.edu.ug.hiczhicz.Circle;
+//import pl.edu.ug.hiczhicz.Ellipse;
+//import pl.edu.ug.hiczhicz.Point;
+//import pl.edu.ug.hiczhicz.Rectangle;
+//import pl.edu.ug.hiczhicz.Shape;
+//
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        Circle c1=new Circle(Color.BLUE, new Point(20,20),10);
+//        System.out.println(c1);
+//        System.out.println(c1.getArea());
+//        System.out.println(c1.getPerimeter());
+//        System.out.println(c1.getCircumscribedSquare(Color.RED));
+//        Shape c2=new Circle(Color.BLUE, new Point(20,20),10);
+//        System.out.println(c2);
+//        //This won't compile, because c2 is declared as Shape.
+//        //c2.getCircumscribedSquare(Color.RED);
+//        //but this will compile ok.
+//        System.out.println(c2);
+//        Shape c3=((Circle) c2).getCircumscribedSquare(Color.RED);
+//        System.out.println(c3);
+//        Rectangle r=new Rectangle(Color.PINK,new Point(15,15),10,20);
+//        System.out.println(r);
+//        System.out.println(r.getCircumscribedCircle(Color.CYAN));
+//        System.out.println(new Ellipse(Color.YELLOW,new Point(10,10),20,30));
+//    }
+//}
 
-import pl.edu.ug.hiczhicz.Circle;
-import pl.edu.ug.hiczhicz.Ellipse;
-import pl.edu.ug.hiczhicz.Point;
-import pl.edu.ug.hiczhicz.Rectangle;
-import pl.edu.ug.hiczhicz.Shape;
+import pl.edu.ug.hiczhicz.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        Circle c1=new Circle(Color.BLUE, new Point(20,20),10);
-        System.out.println(c1);
-        System.out.println(c1.getArea());
-        System.out.println(c1.getPerimeter());
-        System.out.println(c1.getCircumscribedSquare(Color.RED));
-        Shape c2=new Circle(Color.BLUE, new Point(20,20),10);
-        System.out.println(c2);
-        //This won't compile, because c2 is declared as Shape.
-        //c2.getCircumscribedSquare(Color.RED);
-        //but this will compile ok.
-        System.out.println(c2);
-        Shape c3=((Circle) c2).getCircumscribedSquare(Color.RED);
-        System.out.println(c3);
-        Rectangle r=new Rectangle(Color.PINK,new Point(15,15),10,20);
-        System.out.println(r);
-        System.out.println(r.getCircumscribedCircle(Color.CYAN));
-        System.out.println(new Ellipse(Color.YELLOW,new Point(10,10),20,30));
+        MainWindow frame = new MainWindow();
+        frame.setTitle("Shapes");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
---
+++ src/pl/edu/ug/hiczhicz/MainWindow.java
@@ -0,0 +1,118 @@
+package pl.edu.ug.hiczhicz;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
+
+public class MainWindow extends JFrame {
+    private JPanel mainPanel;
+    private JPanel canvasPanel;
+    private JPanel controlPanel;
+    private JButton buttonColor;
+    private JButton buttonShapes;
+    private JButton buttonAdd;
+    private JButton buttonClear;
+    private JButton buttonCredits;
+    private JButton buttonClose;
+    private JLabel pointLabel;
+    private JLabel colorLabel;
+
+    //private DrawingCanvas canvas = new DrawingCanvas();
+    private Color color = null;
+    private Point point = null;
+
+    public MainWindow() {
+        super();
+        this.setContentPane(mainPanel);
+        //canvasPanel.add(canvas);
+        Border greyLine = BorderFactory.createLineBorder(Color.DARK_GRAY);
+        canvasPanel.setBorder(greyLine);
+        canvasPanel.setBackground(new Color(250, 250, 240));
+        controlPanel.setBackground(new Color(245, 245, 250));
+        buttonColor.setText("Choose color");
+        buttonAdd.setText("Add shape");
+        buttonClear.setText("Clear");
+        buttonShapes.setText("List of shapes");
+        buttonCredits.setText("Credits");
+        buttonClose.setText("Close");
+        updateLabels();
+        //drawInitialShapes();
+
+        // All these event listeners should be added by IntelliJ semi-automatically
+        // however in each listener we add our custom method 'action...'
+        // responsible for this event
+        buttonClose.addActionListener(actionEvent -> actionClose());
+        buttonCredits.addActionListener(actionEvent -> actionCredits());
+        canvasPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                actionPointClicked(e);
+            }
+        });
+        buttonColor.addActionListener(actionEvent -> actionChooseColor());
+        buttonClear.addActionListener(actionEvent -> actionClear());
+        buttonAdd.addActionListener(actionEvent -> actionAdd());
+        buttonShapes.addActionListener(actionEvent -> actionShowShapes());
+    }
+
+    private void actionShowShapes() {
+
+    }
+
+    private void actionAdd(){
+
+    }
+
+    private void actionClear(){
+
+    }
+
+    private void actionChooseColor(){
+
+    }
+
+    private void actionPointClicked(MouseEvent e){
+
+    }
+
+    private void actionClose() {
+        this.dispose();
+    }
+
+    private void actionCredits(){
+        JOptionPane.showMessageDialog(this, "Developed by Martyna Pieczka", "Credits", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void updateLabels() {
+
+
+    }
+
+//    private void drawInitialShapes(){
+//        Color color1=new Color(0x84,0xb4,0xc8,0xd0);
+//        Color color2=new Color(0x8e,0xc9,0xbb,0xd0);
+//        Color color3=new Color(0xf4,0xdc,0xd6,0xd0);
+//        Circle c=new Circle(color1,new Point(200,200),70);
+//        canvas.shapes.add(c.getCircumscribedSquare(color3));
+//        canvas.shapes.add(c);
+//        Rectangle r=new Rectangle(color1,new Point(520,350),130,90);
+//        canvas.shapes.add(r.getCircumscribedCircle(color3));
+//        canvas.shapes.add(r);
+//        Ellipse e=new Ellipse(color2,new Point(410,250),90,150);
+//        canvas.shapes.add(e);
+//        canvas.shapes.add(e.getInscribedCircle(color3));
+//        Square s=new Square(color1,new Point(600,80),90);
+//        canvas.shapes.add(s);
+//        canvas.shapes.add(s.getInscribedCircle(color3));
+//        canvas.rePaint();
+//    }
+
+}
+
